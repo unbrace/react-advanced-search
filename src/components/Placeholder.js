1d0f537dfe0e645b16baf12d179fad0f31c8d7d4
Carlo Nomes <carlo.nomes@optis.be>
2018-07-05 12:59:07 +0200

    [fix] do not break "placeholder" text but instead show ellipsis

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -5,6 +5,9 @@ import getTheme from '../utils/getTheme';
 const Placeholder = styled.div`
     font-size: ${getTheme('fontSize')};
     color: ${getTheme('placeHolderColor')};
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
 `;
 
 export default Placeholder;
